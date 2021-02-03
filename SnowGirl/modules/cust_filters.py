39d331ddd8f8984ccee2c4bc9c34c783bcf9ecbb
InukaAsith <inukaasith7@gmail.com>
2021-02-03 11:28:53 +0000

    pylint: auto fixes

--- SnowGirl/modules/cust_filters.py
+++ SnowGirl/modules/cust_filters.py
@@ -2,22 +2,28 @@ import re
 from typing import Optional
 
 import telegram
-from telegram import ParseMode, InlineKeyboardMarkup, Message, Chat
-from telegram import Update, Bot
+from telegram import Bot, Chat, InlineKeyboardMarkup, Message, ParseMode, Update
 from telegram.error import BadRequest
-from telegram.ext import CommandHandler, MessageHandler, DispatcherHandlerStop, run_async
+from telegram.ext import (
+    CommandHandler,
+    DispatcherHandlerStop,
+    MessageHandler,
+    run_async,
+)
 from telegram.utils.helpers import escape_markdown
 
-from SnowGirl import dispatcher, LOGGER, BMERNU_SCUT_SRELFTI
+from SnowGirl import BMERNU_SCUT_SRELFTI, LOGGER, dispatcher
+from SnowGirl.modules.connection import connected
 from SnowGirl.modules.disable import DisableAbleCommandHandler
 from SnowGirl.modules.helper_funcs.chat_status import user_admin
 from SnowGirl.modules.helper_funcs.extraction import extract_text
 from SnowGirl.modules.helper_funcs.filters import CustomFilters
 from SnowGirl.modules.helper_funcs.misc import build_keyboard
-from SnowGirl.modules.helper_funcs.string_handling import split_quotes, button_markdown_parser
+from SnowGirl.modules.helper_funcs.string_handling import (
+    button_markdown_parser,
+    split_quotes,
+)
 from SnowGirl.modules.sql import cust_filters_sql as sql
-
-from SnowGirl.modules.connection import connected
 
 HANDLER_GROUP = 10
 BASIC_FILTER_STRING = "*Filters in this chat:*\n"
@@ -54,13 +60,17 @@ def list_handlers(bot: Bot, update: Update):
     for keyword in all_handlers:
         entry = " - {}\n".format(escape_markdown(keyword))
         if len(entry) + len(filter_list) > telegram.MAX_MESSAGE_LENGTH:
-            update.effective_message.reply_text(filter_list, parse_mode=telegram.ParseMode.MARKDOWN)
+            update.effective_message.reply_text(
+                filter_list, parse_mode=telegram.ParseMode.MARKDOWN
+            )
             filter_list = entry
         else:
             filter_list += entry
 
     if not filter_list == BASIC_FILTER_STRING:
-        update.effective_message.reply_text(filter_list, parse_mode=telegram.ParseMode.MARKDOWN)
+        update.effective_message.reply_text(
+            filter_list, parse_mode=telegram.ParseMode.MARKDOWN
+        )
 
 
 # NOT ASYNC BECAUSE DISPATCHER HANDLER RAISED
@@ -69,7 +79,9 @@ def filters(bot: Bot, update: Update):
     chat = update.effective_chat  # type: Optional[Chat]
     user = update.effective_user  # type: Optional[User]
     msg = update.effective_message  # type: Optional[Message]
-    args = msg.text.split(None, 1)  # use python's maxsplit to separate Cmd, keyword, and reply_text
+    args = msg.text.split(
+        None, 1
+    )  # use python's maxsplit to separate Cmd, keyword, and reply_text
 
     conn = connected(bot, update, chat, user.id)
     if not conn == False:
@@ -111,13 +123,17 @@ def filters(bot: Bot, update: Update):
     is_video = False
     media_caption = None
     has_caption = False
-    content = None # :\
+    content = None  # :\
     buttons = []
 
     # determine what the contents of the filter are - text, image, sticker, etc
     if len(extracted) >= 2:
-        offset = len(extracted[1]) - len(msg.text)  # set correct offset relative to command + notename
-        content, buttons = button_markdown_parser(extracted[1], entities=msg.parse_entities(), offset=offset)
+        offset = len(extracted[1]) - len(
+            msg.text
+        )  # set correct offset relative to command + notename
+        content, buttons = button_markdown_parser(
+            extracted[1], entities=msg.parse_entities(), offset=offset
+        )
         content = content.strip()
         # https://t.me/c/1279877202/3508
 
@@ -128,35 +144,55 @@ def filters(bot: Bot, update: Update):
 
     elif msg.reply_to_message and msg.reply_to_message.document:
         offset = len(msg.reply_to_message.caption or "")
-        media_caption, buttons = button_markdown_parser(msg.reply_to_message.caption, entities=msg.reply_to_message.parse_entities(), offset=offset)
+        media_caption, buttons = button_markdown_parser(
+            msg.reply_to_message.caption,
+            entities=msg.reply_to_message.parse_entities(),
+            offset=offset,
+        )
         content = msg.reply_to_message.document.file_id
         is_document = True
         has_caption = True
 
     elif msg.reply_to_message and msg.reply_to_message.photo:
         offset = len(msg.reply_to_message.caption or "")
-        media_caption, buttons = button_markdown_parser(msg.reply_to_message.caption, entities=msg.reply_to_message.parse_entities(), offset=offset)
+        media_caption, buttons = button_markdown_parser(
+            msg.reply_to_message.caption,
+            entities=msg.reply_to_message.parse_entities(),
+            offset=offset,
+        )
         content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
         is_image = True
         has_caption = True
 
     elif msg.reply_to_message and msg.reply_to_message.audio:
         offset = len(msg.reply_to_message.caption or "")
-        media_caption, buttons = button_markdown_parser(msg.reply_to_message.caption, entities=msg.reply_to_message.parse_entities(), offset=offset)
+        media_caption, buttons = button_markdown_parser(
+            msg.reply_to_message.caption,
+            entities=msg.reply_to_message.parse_entities(),
+            offset=offset,
+        )
         content = msg.reply_to_message.audio.file_id
         is_audio = True
         has_caption = True
 
     elif msg.reply_to_message and msg.reply_to_message.voice:
         offset = len(msg.reply_to_message.caption or "")
-        media_caption, buttons = button_markdown_parser(msg.reply_to_message.caption, entities=msg.reply_to_message.parse_entities(), offset=offset)
+        media_caption, buttons = button_markdown_parser(
+            msg.reply_to_message.caption,
+            entities=msg.reply_to_message.parse_entities(),
+            offset=offset,
+        )
         content = msg.reply_to_message.voice.file_id
         is_voice = True
         has_caption = True
 
     elif msg.reply_to_message and msg.reply_to_message.video:
         offset = len(msg.reply_to_message.caption or "")
-        media_caption, buttons = button_markdown_parser(msg.reply_to_message.caption, entities=msg.reply_to_message.parse_entities(), offset=offset)
+        media_caption, buttons = button_markdown_parser(
+            msg.reply_to_message.caption,
+            entities=msg.reply_to_message.parse_entities(),
+            offset=offset,
+        )
         content = msg.reply_to_message.video.file_id
         is_video = True
         has_caption = True
@@ -166,7 +202,9 @@ def filters(bot: Bot, update: Update):
 
     elif not content:
         # msg.reply_text("You didn't specify what to reply with!")
-        msg.reply_text("There is no note message - You can't JUST have buttons, you need a message to go with it!")
+        msg.reply_text(
+            "There is no note message - You can't JUST have buttons, you need a message to go with it!"
+        )
         return
 
     # print(media_caption)
@@ -179,10 +217,25 @@ def filters(bot: Bot, update: Update):
         if handler.filters == (keyword, chat.id):
             dispatcher.remove_handler(handler, HANDLER_GROUP)
 
-    sql.add_filter(chat_id, keyword, content, is_sticker, is_document, is_image, is_audio, is_voice, is_video,
-                   buttons, media_caption, has_caption)
+    sql.add_filter(
+        chat_id,
+        keyword,
+        content,
+        is_sticker,
+        is_document,
+        is_image,
+        is_audio,
+        is_voice,
+        is_video,
+        buttons,
+        media_caption,
+        has_caption,
+    )
 
-    msg.reply_text("Handler '{}' added in *{}*!".format(keyword, chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
+    msg.reply_text(
+        "Handler '{}' added in *{}*!".format(keyword, chat_name),
+        parse_mode=telegram.ParseMode.MARKDOWN,
+    )
     raise DispatcherHandlerStop
 
 
@@ -216,10 +269,15 @@ def stop_filter(bot: Bot, update: Update):
     for keyword in chat_filters:
         if keyword == args[1]:
             sql.remove_filter(chat_id, args[1])
-            update.effective_message.reply_text("Yep, I'll stop replying to that in *{}*.".format(chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
+            update.effective_message.reply_text(
+                "Yep, I'll stop replying to that in *{}*.".format(chat_name),
+                parse_mode=telegram.ParseMode.MARKDOWN,
+            )
             raise DispatcherHandlerStop
 
-    update.effective_message.reply_text("That's not a current filter - run /filters for all active filters.")
+    update.effective_message.reply_text(
+        "That's not a current filter - run /filters for all active filters."
+    )
 
 
 @run_async
@@ -249,15 +307,40 @@ def reply_filter(bot: Bot, update: Update):
             if filt.is_sticker:
                 message.reply_sticker(filt.reply, reply_markup=keyboard)
             elif filt.is_document:
-                message.reply_document(filt.reply, caption=media_caption, parse_mode=ParseMode.MARKDOWN, reply_markup=keyboard)
+                message.reply_document(
+                    filt.reply,
+                    caption=media_caption,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=keyboard,
+                )
             elif filt.is_image:
-                message.reply_photo(filt.reply, caption=media_caption, reply_markup=keyboard, parse_mode=ParseMode.MARKDOWN)
+                message.reply_photo(
+                    filt.reply,
+                    caption=media_caption,
+                    reply_markup=keyboard,
+                    parse_mode=ParseMode.MARKDOWN,
+                )
             elif filt.is_audio:
-                message.reply_audio(filt.reply, caption=media_caption, parse_mode=ParseMode.MARKDOWN, reply_markup=keyboard)
+                message.reply_audio(
+                    filt.reply,
+                    caption=media_caption,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=keyboard,
+                )
             elif filt.is_voice:
-                message.reply_voice(filt.reply, caption=media_caption, parse_mode=ParseMode.MARKDOWN, reply_markup=keyboard)
+                message.reply_voice(
+                    filt.reply,
+                    caption=media_caption,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=keyboard,
+                )
             elif filt.is_video:
-                message.reply_video(filt.reply, caption=media_caption, parse_mode=ParseMode.MARKDOWN, reply_markup=keyboard)
+                message.reply_video(
+                    filt.reply,
+                    caption=media_caption,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=keyboard,
+                )
             elif filt.has_markdown:
                 keyb = build_keyboard(buttons)
                 keyboard = InlineKeyboardMarkup(keyb)
@@ -267,23 +350,40 @@ def reply_filter(bot: Bot, update: Update):
                     should_preview_disabled = False
 
                 try:
-                    message.reply_text(ad_filter + "\n" + filt.reply, parse_mode=ParseMode.MARKDOWN,
-                                       disable_web_page_preview=should_preview_disabled,
-                                       reply_markup=keyboard)
+                    message.reply_text(
+                        ad_filter + "\n" + filt.reply,
+                        parse_mode=ParseMode.MARKDOWN,
+                        disable_web_page_preview=should_preview_disabled,
+                        reply_markup=keyboard,
+                    )
                 except BadRequest as excp:
                     if excp.message == "Unsupported url protocol":
-                        message.reply_text("You seem to be trying to use an unsupported url protocol. Telegram "
-                                           "doesn't support buttons for some protocols, such as tg://. Please try "
-                                           "again, or ask in @MarieSupport for help.")
+                        message.reply_text(
+                            "You seem to be trying to use an unsupported url protocol. Telegram "
+                            "doesn't support buttons for some protocols, such as tg://. Please try "
+                            "again, or ask in @MarieSupport for help."
+                        )
                     elif excp.message == "Reply message not found":
-                        bot.send_message(chat.id, filt.reply, parse_mode=ParseMode.MARKDOWN,
-                                         disable_web_page_preview=True,
-                                         reply_markup=keyboard)
+                        bot.send_message(
+                            chat.id,
+                            filt.reply,
+                            parse_mode=ParseMode.MARKDOWN,
+                            disable_web_page_preview=True,
+                            reply_markup=keyboard,
+                        )
                     else:
-                        message.reply_text("This note could not be sent, as it is incorrectly formatted. Ask in "
-                                           "@MarieSupport if you can't figure out why!")
-                        LOGGER.warning("Message %s could not be parsed", str(filt.reply))
-                        LOGGER.exception("Could not parse filter %s in chat %s", str(filt.keyword), str(chat.id))
+                        message.reply_text(
+                            "This note could not be sent, as it is incorrectly formatted. Ask in "
+                            "@MarieSupport if you can't figure out why!"
+                        )
+                        LOGGER.warning(
+                            "Message %s could not be parsed", str(filt.reply)
+                        )
+                        LOGGER.exception(
+                            "Could not parse filter %s in chat %s",
+                            str(filt.keyword),
+                            str(chat.id),
+                        )
 
             else:
                 # LEGACY - all new filters will have has_markdown set to True.
@@ -319,7 +419,9 @@ __mod_name__ = "Filters"
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
 LIST_HANDLER = DisableAbleCommandHandler("filters", list_handlers, admin_ok=True)
-CUST_FILTER_HANDLER = MessageHandler(CustomFilters.has_text, reply_filter, edited_updates=True)
+CUST_FILTER_HANDLER = MessageHandler(
+    CustomFilters.has_text, reply_filter, edited_updates=True
+)
 
 dispatcher.add_handler(FILTER_HANDLER)
 dispatcher.add_handler(STOP_HANDLER)
