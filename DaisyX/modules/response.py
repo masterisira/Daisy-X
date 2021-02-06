d1bbe294b8c7229232ec1ac379877e5dee8f21e8
InukaAsith <inukaasith7@gmail.com>
2021-02-06 12:54:53 +0000

    pylint: auto fixes

--- DaisyX/modules/response.py
+++ DaisyX/modules/response.py
@@ -1,9 +1,8 @@
 import aiohttp
 
+
 async def get_response(query):
     async with aiohttp.ClientSession() as ses:
-        async with ses.post(
-            f'https://endpoint.mannu.me/?query={query}'
-        ) as resp:
+        async with ses.post(f"https://endpoint.mannu.me/?query={query}") as resp:
             answer = await resp.json()
-            return answer['response']['bot']
+            return answer["response"]["bot"]
